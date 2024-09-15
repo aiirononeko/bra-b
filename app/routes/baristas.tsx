118928d4a4c2a0ec09f4d8ed0489403a0caf7ca0
aiirononeko <aiirononeko@gmail.com>
2024-09-15 14:27:55 +0900

    :sparkles: baristas index page

--- app/routes/baristas.tsx
+++ app/routes/baristas.tsx
@@ -1,20 +1,47 @@
 import { PrismaClient } from "@prisma/client"
 import { json } from "@remix-run/cloudflare"
 import { useLoaderData } from "@remix-run/react"
+import { Heart, UserRound } from "lucide-react"
 
 export const loader = async () => {
   const prisma = new PrismaClient()
-  const baristas = await prisma.barista.findMany()
+  const baristas = await prisma.barista.findMany({
+    include: {
+      profile: {},
+      store: {}
+    }
+  })
 
   return json({ baristas })
 }
 
 export default function Baristas() {
   const { baristas } = useLoaderData<typeof loader>()
+
   return (
-    <div>
+    <div className='flex flex-col gap-4 max-w-[430px] container'>
       {baristas.map((barista) => (
-        <p key={barista.id}>{barista.id}</p>
+        <div key={barista.id} className='border p-4 space-y-4'>
+          <div className='grid grid-cols-5 gap-6 items-center'>
+            <div className='size-14 bg-gray-100 rounded-full flex items-center justify-center text-gray-300 col-span-1'>
+              <UserRound />
+            </div>
+            <div className='col-span-3'>
+              <p>{barista.profile?.name}</p>
+              <div className='flex gap-2 items-baseline'>
+                <p className='text-sm'>バリスタ歴</p>
+                <p>{barista.profile?.yearsExperience}年</p>
+              </div>
+            </div>
+            <div className='col-span-1 flex flex-col justify-center items-center'>
+              <Heart />
+              <span className='text-gray-400 text-sm'>256</span>
+            </div>
+          </div>
+          <div>
+            <p>{barista.store?.name}</p>
+          </div>
+        </div>
       ))}
     </div>
   )

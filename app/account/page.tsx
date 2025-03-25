54d749c77e1fc2123d367f28a2b46e9ae6e87fc3
aiirononeko <aiirononeko@gmail.com>
2025-03-26 01:53:38 +0900

    biome setup

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,5 +1,5 @@
-import AccountForm from "./account-form";
 import { createClient } from "@/utils/supabase/server";
+import AccountForm from "./account-form";
 
 export default async function Account() {
 	const supabase = await createClient();
